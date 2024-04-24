4838cb11bc339441d738b45302e60e96e3db8131
NadimSrabon <srabonnadim78@gmail.com>
2024-04-24 22:44:21 +0600

    second last

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const Users = () => {
 
     const userInformation = useLoaderData();
+
+    const [users , setUsers] = useState(userInformation);
+
+    function handleItemDelete(id){
+        fetch(`http://localhost:5000/user/${id}`, {
+            method: 'DELETE',
+           
+        })
+        .then(res => res.json())
+        .then(data => {
+            if(data.deletedCount > 0){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'User Deleted Successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                })
+
+                const remainingUsers = users.filter(user => user._id !== id);
+                fetch("http://localhost:5000/user")
+                .then(res => res.json())
+                .then(data => {
+                    setUsers(data);
+                })
+                
+
+
+
+            }
+        })
+    }
 
 
     return (
@@ -20,16 +52,20 @@ const Users = () => {
                             <th></th>
                             <th>Email</th>
                             <th>Created At</th>
+                            <th>Last Logged In</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            userInformation.map(user => <tr key={user._id}>
+                            users.map(user => <tr key={user._id}>
                                 <th>1</th>
                                 <td>{user.email}</td>
                                 <td>{user.createdAt}</td>
-                                <td>Blue</td>
+                                <td></td>
+                                <td>
+                                    <button onClick={() => handleItemDelete(user._id)} className='btn'>X</button>
+                                </td>
                             </tr>)
                         }
                     </tbody>
